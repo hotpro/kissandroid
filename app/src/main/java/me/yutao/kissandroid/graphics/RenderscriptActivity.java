93dc9ccaf681c727901e0da85225ab57ecb237e4
chris.hou <yutao@yutao-mbp.local>
2014-05-26 19:00:05 +0800

    graphics

---
+++ app/src/main/java/me/yutao/kissandroid/graphics/RenderscriptActivity.java
@@ -0,0 +1,87 @@
+package me.yutao.kissandroid.graphics;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import me.yutao.kissandroid.BaseActivity;
+import me.yutao.kissandroid.R;
+
+public class RenderscriptActivity extends BaseActivity {
+
+    private ImageView imageBlur;
+    private ImageView imageOrigin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renderscript);
+        imageOrigin = (ImageView) findViewById(R.id.imageOrigin);
+        imageOrigin.setImageResource(R.drawable.blur_1);
+        imageBlur = (ImageView) findViewById(R.id.imageBlur);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blur_1);
+        imageBlur.setImageBitmap(blurBitmap(bitmap));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.renderscript, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Bitmap blurBitmap(Bitmap bitmap) {
+
+        //Let's create an empty bitmap with the same size of the bitmap we want to blur
+        Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Instantiate a new Renderscript
+        RenderScript rs = RenderScript.create(getApplicationContext());
+
+        //Create an Intrinsic Blur Script using the Renderscript
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+        //Create the in/out Allocations with the Renderscript and the in/out bitmaps
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
+        Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
+
+        //Set the radius of the blur
+        blurScript.setRadius(25.f);
+
+        //Perform the Renderscript
+        blurScript.setInput(allIn);
+        blurScript.forEach(allOut);
+
+        //Copy the final bitmap created by the out Allocation to the outBitmap
+        allOut.copyTo(outBitmap);
+
+        //recycle the original bitmap
+        bitmap.recycle();
+
+        //After finishing everything, we destroy the Renderscript.
+        rs.destroy();
+        return outBitmap;
+    }
+
+}
